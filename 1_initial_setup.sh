5737745c11912966c510394538847376a3d2f9b0
SySfRaMe <32898648+SySfRaMe@users.noreply.github.com>
2020-11-02 18:41:39 +0100

    Update 1_initial_setup.sh

--- 1_initial_setup.sh
+++ 1_initial_setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "download image and burn to sd-card"
+echo "add a file called ssh in the boot folder"
+echo "plugin rpi and boot"
 echo "uncomment/comment commands at your desire"
 echo "change your initial password for raspbian"
 passwd
